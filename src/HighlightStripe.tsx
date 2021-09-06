0a8b5a233f1b72762b188722465aa7c8d9665417
Endi Zhupani <Endi.Zhupani@ximea.onmicrosoft.com>
2021-09-06 14:20:01 +0200

    fixed some linting errros

--- src/HighlightStripe.tsx
+++ src/HighlightStripe.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 export const HighlightStripe = () => {
-	return (
-		<div
-			style={{
-				height: "100%",
-				background: "linear-gradient(0deg, rgba(208,75,13,1) 0%, rgba(240,99,0,1) 100%)",
-				minWidth: "6px",
-				borderRadius: "3px",
-				marginRight: ".75em",
-			}}
-		></div>
-	);
+  return (
+    <div
+      style={{
+        height: "100%",
+        background:
+          "linear-gradient(0deg, rgba(208,75,13,1) 0%, rgba(240,99,0,1) 100%)",
+        minWidth: "6px",
+        borderRadius: "3px",
+        marginRight: ".75em",
+      }}
+    ></div>
+  );
 };
 
 export const WithHighlightStripe = (props: React.ComponentProps<"div">) => {
-	return (
-		<div {...props} style={{ display: "flex", alignItems: "center", ...props.style }}>
-			<HighlightStripe></HighlightStripe>
-			{props.children}
-		</div>
-	);
+  return (
+    <div
+      {...props}
+      style={{ display: "flex", alignItems: "center", ...props.style }}
+    >
+      <HighlightStripe></HighlightStripe>
+      {props.children}
+    </div>
+  );
 };
