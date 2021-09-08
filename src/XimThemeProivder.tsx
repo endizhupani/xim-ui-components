5d08dfd1a23c5b208a14ba064188ae64337e4b36
Endi Zhupani <Endi.Zhupani@ximea.onmicrosoft.com>
2021-09-08 11:43:04 +0200

    feat(ximthemeprovider): added a theme provider
    Added the theme provider with the Ximea Corporate colors

---
+++ src/XimThemeProivder.tsx
@@ -0,0 +1,75 @@
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import { FunctionComponent } from "react";
+
+const theme = createTheme({
+	palette: {
+		type: "dark",
+		secondary: {
+			main: "#ec6608",
+			dark: "#ec6608",
+		},
+		primary: {
+			main: "#ec6608",
+			dark: "#ec6608",
+		},
+
+		background: {
+			paper: "#3d3d3d",
+			default: "#252525",
+		},
+
+		text: {
+			primary: "#fff",
+			disabled: "#7d7d7d",
+			hint: "#7d7d7d",
+			secondary: "#ec6608",
+		},
+		action: {
+			disabled: "#7d7d7d",
+		},
+	},
+	breakpoints: {
+		values: {
+			xs: 0,
+			sm: 850,
+			md: 960,
+			lg: 1280,
+			xl: 1920,
+		},
+	},
+	typography: {
+		fontSize: 14,
+		body1: {
+			fontSize: "14px",
+		},
+		h1: {
+			fontFamily: "BenchNine",
+			textTransform: "uppercase",
+		},
+		h2: {
+			fontFamily: "BenchNine",
+			textTransform: "uppercase",
+		},
+		h3: {
+			fontFamily: "BenchNine",
+			textTransform: "uppercase",
+		},
+		h4: {
+			fontFamily: "BenchNine",
+			textTransform: "uppercase",
+		},
+	},
+	overrides: {
+		MuiCssBaseline: {
+			"@global": {
+				body: {
+					fontSize: "1rem",
+				},
+			},
+		},
+	},
+});
+
+export const XimeaProcessStep: FunctionComponent<{}> = (props) => {
+	return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
+};
